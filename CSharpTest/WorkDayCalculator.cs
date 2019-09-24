4754b83935835d958d04e8b3ccef1852b0b9b44d
Pavel M <satlit1c@gmail.com>
2019-09-24 15:32:04 +0300

    start

---
+++ CSharpTest/WorkDayCalculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTest
+{
+    public static class WorkDayCalculator 
+    {
+        public static DateTime Calculate(DateTime startDate, int dayCount, params WeekEnd[] Weekend)
+        {
+            DateTime EndDate = startDate;
+            for (int i =1; i <= dayCount-1; i++)
+            {
+
+                if (Weekend != null)
+                {
+                    foreach (var item in Weekend)
+                    {
+                        if (EndDate.AddDays(1) >= item.StartDate && EndDate <= item.EndDate)
+                        {
+                            EndDate = item.EndDate;
+                            break;
+                        }
+                    }
+                }
+                EndDate = EndDate.AddDays(1);            
+            }
+            return EndDate;
+        }
+    }
+}
---
+++ WorkDayCalculatorTestConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharpTest;
+
+
+//add console app
+namespace WorkDayCalculatorTestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+            var Weekend = new WeekEnd(new DateTime(2017, 4, 23), new DateTime(2017, 4, 25));
+            var EndDate = WorkDayCalculator.Calculate(new DateTime(2017, 4, 21), 10, Weekend);
+            Console.WriteLine(EndDate);
+            Console.ReadKey();
+        }
+    }
+}
